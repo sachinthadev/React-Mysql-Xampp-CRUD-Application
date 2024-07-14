9ac52189d0719a6573c2714225331cbce9917a9d
sachinthadev <sahansachintha.dev@gmail.com>
2024-07-14 17:36:15 +0530

    crud query created.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require("mysql");
 const server = express();
-
+server.use(bodyParser.json());
 
 //DB Connection
 
@@ -28,7 +28,7 @@ db.connect(function (error) {
   server.listen(8085,function check(error) {
     if (error) 
     {
-    console.log("Error....dddd!!!!");
+    console.log("Error....!!!!");
     }
 
     else 
@@ -37,3 +37,81 @@ db.connect(function (error) {
 
     }
 });
+
+
+
+server.post("/api/student/add", (req, res) => {
+    let details = {
+      stname: req.body.stname,
+      course: req.body.course,
+      phone: req.body.phone,
+    };
+    let sql = "INSERT INTO student SET ?";
+    db.query(sql, details, (error) => {
+      if (error) {
+        res.send({ status: false, message: "Student created Failed" });
+      } else {
+        res.send({ status: true, message: "Student created successfully" });
+      }
+    });
+  });
+
+
+  server.get("/api/student", (req, res) => {
+    var sql = "SELECT * FROM student";
+    db.query(sql, function (error, result) {
+      if (error) {
+        console.log("Error Connecting to DB");
+      } else {
+        res.send({ status: true, data: result });
+      }
+    });
+  });
+
+
+
+  server.get("/api/student/:id", (req, res) => {
+    var studentid = req.params.id;
+    var sql = "SELECT * FROM student WHERE id=" + studentid;
+    db.query(sql, function (error, result) {
+      if (error) {
+        console.log("Error Connecting to DB");
+      } else {
+        res.send({ status: true, data: result });
+      }
+    });
+  });
+
+
+  server.put("/api/student/update/:id", (req, res) => {
+    let sql =
+      "UPDATE student SET stname='" +
+      req.body.stname +
+      "', course='" +
+      req.body.course +
+      "',phone='" +
+      req.body.phone +
+      "'  WHERE id=" +
+      req.params.id;
+  
+    let a = db.query(sql, (error, result) => {
+      if (error) {
+        res.send({ status: false, message: "Student Updated Failed" });
+      } else {
+        res.send({ status: true, message: "Student Updated successfully" });
+      }
+    });
+  });
+
+
+
+  server.delete("/api/student/delete/:id", (req, res) => {
+    let sql = "DELETE FROM student WHERE id=" + req.params.id + "";
+    let query = db.query(sql, (error) => {
+      if (error) {
+        res.send({ status: false, message: "Student Deleted Failed" });
+      } else {
+        res.send({ status: true, message: "Student Deleted successfully" });
+      }
+    });
+  });
